cf6e32aa64934545381752af1abdb4fdaea75405
Pablo Jesús González Rubio <pjgr234@usal.es>
2020-04-27 20:13:39 +0200

    Update

--- Ranas-Win/Ranas-Win.cpp
+++ Ranas-Win/Ranas-Win.cpp
@@ -1,35 +1,72 @@
 #include <stdio.h> 
 #include <stdlib.h>
 #include <Windows.h>
+#include <iostream>
 #include "ranas.h"
 
-int main(int argc, char * argv[])
+#define HOR_MIN 0
+#define HOR_MAX 79
+#define VER_MIN 0
+#define VER_MAX 11
+
+//(Y = 0 y 3) == antes de cruzar el río.
+//(Y = 11)    == orilla
+
+
+/* Punteros a las funciones */
+BOOL * AvanceRana(int*, int*, int);
+BOOL * AvanceRanaFin(int, int);
+BOOL * AvanceRanaIni(int, int);
+BOOL * AvanceTroncos(int i);
+BOOL * ComprobarEstadIsticas(LONG, LONG, LONG);
+BOOL * FinRanas(void);
+BOOL * InicioRanas(int, int*, int*, int*, int, TIPO_CRIAR);
+BOOL * PartoRanas(int);
+void * Pausa(void);
+BOOL * PuedoSaltar(int, int, int);
+void * PrintMsg(char*);
+
+//FUNCIONES
+void tratarArg(int argc, char* argv[]);
+int cargarRanas();
+
+//MAIN
+int main(int argc, char* argv[])
 {
+	/*
 	HANDLE hilo1, hilo2;
 	CHAR char1, char2;
 	DWORD id1, id2;
 	INT i, reps;
+	
+	*/
 
 	//TRATAMIENTO ARGUMENTOS
-	//...
+	tratarArg(argc, argv);
 
+	if (cargarRanas() == -1) {
+		PERROR("Error al cargar la biblioteca ranas.");
+		exit(2);
+	}
+
+	/*
 	//PRIORIDAD DEL PROCESO NORMAL --> IDLE
 	if (SetPriorityClass(GetCurrentProcess(), IDLE_PRIORITY_CLASS) == false) {
-		perror("SetPriorityClass");
+		PERRORR("SetPriorityClass");
 		return 1;
 	}
 
 	//CREACIÓN HILO 1
-	hilo1 = CreateThread(NULL, 0, funcion_hilo1, (void*)reps, NULL, &id1);
+	hilo1 = CreateThread(NULL, 0, funcionHilo1, (void*)reps, NULL, &id1);
 	if (SetThreadPriority(hilo1, THREAD_PRIORITY_HIGHEST) == false) {
-		perror("SetThreadPriority");
+		PERRORR("SetThreadPriority");
 		return 1;
 	}
 
 	//CREACIÓN HILO 2
-	hilo2 = CreateThread(NULL, 0, funcion_hilo1, (void*)reps, NULL, &id2);
+	hilo2 = CreateThread(NULL, 0, funcionHilo2, (void*)reps, NULL, &id2);
 	if (SetThreadPriority(hilo2, THREAD_PRIORITY_HIGHEST) == false) {
-		perror("SetThreadPriority");
+		PERRORR("SetThreadPriority");
 		return 1;
 	}
 
@@ -42,29 +79,62 @@ int main(int argc, char * argv[])
 
 	Sleep(4000); //Para que el padre no se muera, chapuza
 	//Para esperar a los hijos 
+	*/
 	return 0;
 }
 
+//FUNCION TRATAR ARGUMENTOS
+void tratarArg(int argc, char * argv[]) {
 
+int param1, param2;
+
+
+if(argc != 3) {
+	fprintf(stderr,"%s\n","USO: ./ranas VELOCIDAD VELOCIDAD_PARTO");
+	exit(1);
+}
+
+param1 = atoi(argv[1]);
+param2 = atoi(argv[2]);
+
+if(param1 < 0 || param1 > 1000) {
+	fprintf(stderr,"%s\n","Ha introducido una velocidad incorrecta.\nPor favor, introduzca una velocidad ente 0 y 1000.");
+	exit(1);
+}
+
+if (param2 <= 0) {
+	fprintf(stderr, "%s\n", "Ha introducido un tiempo de partos incorrecto.\nIntroduzca un tiempo mayor de 0.");
+	exit(1);
+}
+
+}
+
+
+//FUNCION CARGAR LIBRERIA RANAS.DLL
+int cargarRanas() {
+	HINSTANCE controladorDll;
+
+	controladorDll = LoadLibrary("ranas.dll");
+	if (NULL == controladorDll) {
+		fprintf(stderr, "%s\n", "ErrorLoadLibrary");
+		return -1;
+	}
+
+	BOOL InicioRanas(int delta_t, int lTroncos[], int lAguas[], int dirs[], int t_Criar, TIPO_CRIAR f_Criar);
+}
+
+/*
 //FUNCION DEL HILO 1
 DWORD WINAPI funcionHilo1(LPVOID argumento) {
 	INT i, reps;
 	//...
+	return 0;
 }
-
 
 //FUNCION DEL HILO 2
 DWORD WINAPI funcionHilo2(LPVOID argumento) {
 	INT i, reps;
 	//...
+	return 0;
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+*/
