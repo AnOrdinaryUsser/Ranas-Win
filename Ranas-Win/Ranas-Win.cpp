786393fc77dad58f354e74b6d940ef3d19fca9af
Pablo Jesús González Rubio <pjgr234@usal.es>
2020-04-29 11:32:41 +0200

    Update

--- Ranas-Win/Ranas-Win.cpp
+++ Ranas-Win/Ranas-Win.cpp
@@ -8,70 +8,101 @@
 #define HOR_MAX 79
 #define VER_MIN 0
 #define VER_MAX 11
+#define MSG_ERROR "GetProcAddress Error."
 
-//CREAR STRUCT IPC RANAS
-HINSTANCE globalRanasDLL;
+/*
+struct ranasInicio {
+	//delta_t: valor del tic de reloj en milisegundos.
+	int delta_t;
+	//lTroncos : array de siete enteros que contiene el valor de la longitud media de los troncos para cada fila.
+	//El índice cero del array se refiere a la fila superior de troncos.
+	int lTroncos[];
+	//lAguas : Referido a la longitud media del espacio entre troncos.
+	int lAguas[];
+	//dirs : Cada elemento puede valer DERECHA(0) o IZQUIERDA(1), indicando la dirección en que se moverán los troncos.
+	int dirs[];
+	//t_Criar : tiempo de reposo entre dos partos, expresado en tics.
+	int t_Criar;
+	//f_Criar : puntero a la función de rellamada que será invocada cada vez que una rana madre deba reproducirse.
+	//El prototipo de la función tiene que ser de modo que devuelva void y reciba un argumento entero, la posición de la rana madre que debe parir.
+	TIPO_CRIAR f_Criar;
+};*/
+
 
 //(Y = 0 y 3) == antes de cruzar el río.
 //(Y = 11)    == orilla
 
-
 /* Punteros a las funciones */
-BOOL (* AvanceRana1)(int*, int*, int);
-BOOL (* AvanceRanaFin1)(int, int);
-BOOL (* AvanceRanaIni1)(int, int);
-BOOL (* AvanceTroncos1)(int i);
-BOOL (* ComprobarEstadIsticas1)(LONG, LONG, LONG);
-BOOL (* FinRanas1)(void);
-BOOL (* InicioRanas1)(int, int*, int*, int*, int, TIPO_CRIAR);
-BOOL (* PartoRanas1)(int);
-void (* Pausa1)(void);
-BOOL (* PuedoSaltar1)(int, int, int);
-void (* PrintMsg1)(char*);
+typedef BOOL(* AvanceRana)(int*, int*, int);
+typedef BOOL(* AvanceRanaFin)(int, int);
+typedef BOOL(* AvanceRanaIni)(int, int);
+typedef BOOL(* AvanceTroncos)(int i);
+typedef BOOL(* ComprobarEstadIsticas)(LONG, LONG, LONG);
+typedef BOOL(* FinRanas)(void);
+typedef BOOL(* InicioRanas)(int, int*, int*, int*, int, TIPO_CRIAR);
+typedef BOOL(* PartoRanas)(int);
+typedef void(* Pausa)(void);
+typedef BOOL(* PuedoSaltar)(int, int, int);
+typedef void(* PrintMsg)(char*);
+
+struct funcionesDLL {
+	HINSTANCE ranasDLL;
+	AvanceRana avanceRana;
+	AvanceRanaIni avanceRanaIni;
+	AvanceRanaFin avanceRanaFin;
+	AvanceTroncos avanceTroncos;
+	ComprobarEstadIsticas comprobarEstadisticas;
+	FinRanas finRanas;
+	InicioRanas inicioRanas;
+	PartoRanas partoRanas;
+	PuedoSaltar puedoSaltar;
+} funciones;
 
 //FUNCIONES
-void tratarArg(int argc, char* argv[]);
+void tratarArg(int argc, char* argv[], int*, int*);
 int cargarRanas();
+int error(BOOL, const char *);
 
 //MAIN
 int main(int argc, char* argv[])
 {
 	system("mode con:cols=80 lines=25"); //FIJA AUTOMÁTICAMENTE A 80x25
-	/*
-	HANDLE hilo1, hilo2;
+
+	/*HANDLE hilo1, hilo2;
 	CHAR char1, char2;
 	DWORD id1, id2;
 	INT i, reps;
-	
 	*/
+	int *velocidad, *parto;
 
 	//TRATAMIENTO ARGUMENTOS
-	tratarArg(argc, argv);
+	tratarArg(argc, argv, velocidad, parto);
 
 	if (cargarRanas() == -1) {
 		PERROR("Error al cargar la biblioteca ranas.");
 		exit(2);
 	}
-	
-	Sleep(5000);
+
+
+
 	/*
 	//PRIORIDAD DEL PROCESO NORMAL --> IDLE
 	if (SetPriorityClass(GetCurrentProcess(), IDLE_PRIORITY_CLASS) == false) {
-		PERRORR("SetPriorityClass");
+		PERROR("SetPriorityClass");
 		return 1;
 	}
 
 	//CREACIÓN HILO 1
 	hilo1 = CreateThread(NULL, 0, funcionHilo1, (void*)reps, NULL, &id1);
 	if (SetThreadPriority(hilo1, THREAD_PRIORITY_HIGHEST) == false) {
-		PERRORR("SetThreadPriority");
+		PERROR("SetThreadPriority");
 		return 1;
 	}
 
 	//CREACIÓN HILO 2
 	hilo2 = CreateThread(NULL, 0, funcionHilo2, (void*)reps, NULL, &id2);
 	if (SetThreadPriority(hilo2, THREAD_PRIORITY_HIGHEST) == false) {
-		PERRORR("SetThreadPriority");
+		PERROR("SetThreadPriority");
 		return 1;
 	}
 
@@ -81,15 +112,14 @@ int main(int argc, char* argv[])
 		printf("0");
 		fflush(stdout);
 	}
-
-	Sleep(4000); //Para que el padre no se muera, chapuza
-	//Para esperar a los hijos 
 	*/
+	Sleep(4000); // Se debe esperar 30 segundos para finalizar el programa
+	
 	return 0;
 }
 
 //FUNCION TRATAR ARGUMENTOS
-void tratarArg(int argc, char * argv[]) {
+void tratarArg(int argc, char * argv[], int *velocidad, int *parto) {
 
 int param1, param2;
 
@@ -102,97 +132,57 @@ if(argc != 3) {
 param1 = atoi(argv[1]);
 param2 = atoi(argv[2]);
 
-if(param1 < 0 || param1 > 1000) {
-	fprintf(stderr,"%s\n","Ha introducido una velocidad incorrecta.\nPor favor, introduzca una velocidad ente 0 y 1000.");
+if (param1 < 0 || param1 > 1000) {
+	fprintf(stderr, "%s\n", "Ha introducido una velocidad incorrecta.\nPor favor, introduzca una velocidad ente 0 y 1000.");
 	exit(1);
-}
+} *velocidad = param1;
 
 if (param2 <= 0) {
 	fprintf(stderr, "%s\n", "Ha introducido un tiempo de partos incorrecto.\nIntroduzca un tiempo mayor de 0.");
 	exit(1);
-}
+} *parto = param2;
 
 }
 
 
 //FUNCION CARGAR LIBRERIA RANAS.DLL
 int cargarRanas() {
-	HINSTANCE ranasDLL;
 
-	ranasDLL = LoadLibrary("ranas.dll");
-	if (NULL == ranasDLL) {
-		PERROR("Error al cargar ranas.dll (LoadLibrary).");
-		return -1;
-	}
-
-	globalRanasDLL = ranasDLL; //Guardamos el puntero del fichero DLL
-
-	AvanceRana1 = (BOOL(*)(int *,int *,int)) GetProcAddress(ranasDLL, "AvanceRana");
-	if (AvanceRana1 == NULL) {
-		PERROR("Error en GetProcAddress: AvanceRana.");
-		return -1;
-	}
-
-	AvanceRanaFin1 = (BOOL(*)(int, int)) GetProcAddress(ranasDLL, "AvanceRanaFin");
-	if (AvanceRanaFin1 == NULL) {
-		PERROR("Error en GetProcAddress: AvanceRanaFin.");
-		return -1;
-	}
-
-	AvanceRanaIni1 = (BOOL(*)(int, int)) GetProcAddress(ranasDLL, "AvanceRanaIni");
-	if (AvanceRanaIni1 == NULL) {
-		PERROR("Error en GetProcAddress: AvanceRanaIni.");
-		return -1;
-	}
-
-	AvanceTroncos1 = (BOOL(*)(int i)) GetProcAddress(ranasDLL, "AvanceTroncos");
-	if (AvanceTroncos1 == NULL) {
-		PERROR("Error en GetProcAddress: AvanceTroncos.");
-		return -1;
-	}
-
-	ComprobarEstadIsticas1 = (BOOL(*)(LONG, LONG, LONG)) GetProcAddress(ranasDLL, "ComprobarEstadIsticas");
-	if (ComprobarEstadIsticas1 == NULL) {
-		PERROR("Error en GetProcAddress: ComprobarEstadIsticas.");
-		return -1;
-	}
-
-	FinRanas1 = (BOOL(*)(void)) GetProcAddress(ranasDLL, "FinRanas");
-	if (FinRanas1 == NULL) {
-		PERROR("Error en GetProcAddress: FinRanas.");
-		return -1;
-	}
-
-	InicioRanas1 = (BOOL(*)(int, int*, int*, int*, int, TIPO_CRIAR)) GetProcAddress(ranasDLL, "InicioRanas");
-	if (InicioRanas1 == NULL) {
-		PERROR("Error en GetProcAddress: InicioRanas.");
-		return -1;
-	}
-
-	PartoRanas1 = (BOOL(*)(int)) GetProcAddress(ranasDLL, "PartoRanas");
-	if (PartoRanas1 == NULL) {
-		PERROR("Error en GetProcAddress: PartoRanas.");
-		return -1;
-	}
+	if(error(funciones.ranasDLL = LoadLibrary("ranas.dll") , "")) return 1;
+	if(error(funciones.avanceRana = (BOOL(*)(int *,int *,int)) GetProcAddress(funciones.ranasDLL, "AvanceRana"), "AvanceRana")) return 1;
+	if(error(funciones.avanceRanaFin = (BOOL(*)(int, int)) GetProcAddress(funciones.ranasDLL, "AvanceRanaFin"), "AvanceRanaFin")) return 1;
+	if (error(funciones.avanceRanaIni = (BOOL(*)(int, int)) GetProcAddress(funciones.ranasDLL, "AvanceRanaIni"), "AvanceRanaIni")) return 1;
+	if (error(funciones.avanceTroncos = (BOOL(*)(int i)) GetProcAddress(funciones.ranasDLL, "AvanceTroncos"), "AvanceTroncos")) return 1;
+	if (error(funciones.comprobarEstadisticas = (BOOL(*)(LONG, LONG, LONG)) GetProcAddress(funciones.ranasDLL, "ComprobarEstadIsticas"), "ComprobarEstadisticas")) return 1;
+	if (error(funciones.finRanas = (BOOL(*)(void)) GetProcAddress(funciones.ranasDLL, "FinRanas"), "FinRanas")) return 1;
+	if (error(funciones.inicioRanas = (BOOL(*)(int, int*, int*, int*, int, TIPO_CRIAR)) GetProcAddress(funciones.ranasDLL, "InicioRanas"), "InicioRanas")) return 1;
+	if (error(funciones.partoRanas = (BOOL(*)(int)) GetProcAddress(funciones.ranasDLL, "PartoRanas"), "PartoRanas")) return 1;
+	
+	/*
 	Pausa1 = (void(*)(void)) GetProcAddress(ranasDLL, "Pausa");
 	if (Pausa1 == NULL) {
 		PERROR("Error en GetProcAddress: Pausa.");
 		return -1;
-	}
+	}*/
 
-	PuedoSaltar1 = (BOOL(*)(int, int, int)) GetProcAddress(ranasDLL, "PuedoSaltar");
-	if (PuedoSaltar1 == NULL) {
-		PERROR("Error en GetProcAddress: PuedoSaltar.");
-		return -1;
-	}
+	if (error(funciones.puedoSaltar = (BOOL(*)(int, int, int)) GetProcAddress(funciones.ranasDLL, "PuedoSaltar"),"PuedoSaltar")) return 1;
 	
+	
+	/*
 	PrintMsg1 = (void(*)(char*)) GetProcAddress(ranasDLL, "PrintMsg");
 	if (PrintMsg1 == NULL) {
 		PERROR("Error enGetProcAddress: PrintMsg.");
 		return -1;
-	}
+	}*/
 
 	return 0;
+}
+
+int error(BOOL ret, const char MSG_FUNCTION[]) {
+	if (ret == FALSE) {
+		fprintf(stderr,"%s: %s.\n",MSG_ERROR,MSG_FUNCTION);
+		return 1;
+	} else return 0;
 }
 
 /*
