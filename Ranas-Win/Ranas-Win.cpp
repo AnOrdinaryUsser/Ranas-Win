bd36afcb7080287f965da0afa809c51b61d6785d
Pablo Jesús González Rubio <pjgr234@usal.es>
2020-05-08 19:00:39 +0200

    Código Limpio

--- Ranas-Win/Ranas-Win.cpp
+++ Ranas-Win/Ranas-Win.cpp
@@ -2,15 +2,14 @@
    Fichero: ranas.cpp
    Integrantes del grupo:	Pablo Jesús González Rubio - i0894492
 							Sergio García González - i0921911
-   Fecha de modificación: 
+   Fecha de modificación:
  */
 
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
 #include <iostream>
 #include "ranas.h"
-#include <time.h>
 
 typedef BOOL DESTINO, *PDESTINO;
 typedef HANDLE TRONCOS, *PTRONCOS, ORILLA, *PORILLA;
@@ -23,8 +22,6 @@ typedef HANDLE TRONCOS, *PTRONCOS, ORILLA, *PORILLA;
 #define ORILLA_MAX 3
 #define TRONCOS_MIN 4
 #define TRONCOS_MAX 10
-#define MSG_ERROR "GetProcAddress FERROR."
-#define THREAD_MAX 10000
 
 
 #define FERROR(ReturnValue,ErrorValue,ErrorMsg)							\
@@ -48,7 +45,7 @@ struct funcionesDLL {
 	TIPO_INICIORANAS inicioRanas;
 	TIPO_PARTORANAS partoRanas;
 	TIPO_PUEDOSALTAR puedoSaltar;
-	
+
 	TIPO_PAUSA pausa;
 	TIPO_PRINTMSG printMSG;
 } funciones;
@@ -63,15 +60,7 @@ void avanzarRana(int *posX, int *posY, int dir);
 
 //VAR GLOBALES
 long * nacidas, * salvadas, * perdidas;
-int nMadre = 0;
-int noTerminado = 1;
-int posicion;
-//Memoria Compartida
-//Semáforos
-
-//HILOS
-HANDLE hilos[THREAD_MAX];
-DWORD hilosID;
+int posicion; //Se utiliza para pasarle "pos" a los movXY de moverRanas desde f_criar
 
 //MUTEXES DEBUG
 HANDLE ranasMutex;
@@ -89,28 +78,16 @@ int main(int argc, char* argv[])
 	int lTroncos[] = { 4,5,4,5,4,5,4 };
 	int lAguas[] = { 5,4,3,5,3,4,5 };
 	int dirs[] = { 1,0,1,0,1,0,1 };
-	int i,* posX, * posY;
-	
-	//int nRana, nProcesos, nTroncos, param1, param2;
-	int * movX, * movY; //Probar a quitar el global
+	int i;
 
 	//TRATAMIENTO ARGUMENTOS
 	tratarArg(argc, argv);
 	velocidad = atoi(argv[1]);
 	parto = atoi(argv[2]);
 
-	FERROR(cargarRanas(),-1,"ERROR al cargar la biblioteca ranas.");
-
-	//CREACIÓN MEMORIA COMPARTIDA
-
-	//¿CREACIÓN PUNTERO MEMORIA COMPARTIDA?
+	FERROR(cargarRanas(),-1,"cargarRanas() ERROR.\n");
 
 	//INICIALIZACIÓN MEMORIA
-	posX = (PINT)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(posX) * 8);
-	//Los posX y posY a lo mejor necesitamos inicializar tantas veces como hilos haya
-	posY = (PINT)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(posY) * 8 + 4);
-	*posX = -1;
-	*posY = -1;
 	nacidas = (PLONG)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(nacidas) * 8);
 	salvadas = (PLONG)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(salvadas) * 8);
 	perdidas = (PLONG)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(perdidas) * 8);
@@ -119,41 +96,41 @@ int main(int argc, char* argv[])
 	//CREACIÓN RECURSOS DEBUG
 	FERROR(ranasMutex = CreateMutex(NULL, FALSE, NULL),NULL, "CreateMutex() ERROR.\n");
 
-	funciones.inicioRanas(velocidad, lTroncos, lAguas, dirs, parto, f_criar);
-	
+	FERROR(funciones.inicioRanas(velocidad, lTroncos, lAguas, dirs, parto, f_criar),FALSE,"inicioRanas() ERROR.\n");
+
 	Sleep(30000); // Se debe esperar 30 segundos para finalizar el programa
 
-	funciones.finRanas();
+	FERROR(funciones.finRanas(),NULL,"finRanas() ERROR.\n");
 
-	//Acabar con hilos de ranas
-	WaitForMultipleObjects(THREAD_MAX, hilos, TRUE, INFINITE);
-	for (i = 0; i < THREAD_MAX; i++) CloseHandle(hilos[i]);
-	CloseHandle(ranasMutex);
+	FERROR(CloseHandle(ranasMutex),NULL,"CloseHandle() ERROR.\n");
 
-	funciones.comprobarEstadisticas(*nacidas, *salvadas, *perdidas);
-	
+	FERROR(funciones.comprobarEstadisticas(*nacidas, *salvadas, *perdidas),NULL,"comprobarEstadisticas() ERROR.\n");
+
 	return 0;
 }
 
 // Funcion f_criar
 void f_criar (int pos) { //Llamará a la función PartoRanas, actualiza las estadísticas, crea un nuevo hilo para mover a la recién nacida y mueve los troncos
-	
+
 	//pos = 0; //DEBUG (PARA QUE SOLO PARA LA RANA MADRE 0)
-	int* movX, *movY;
-	movX = (PINT)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(movX) * 8);
-	movY = (PINT)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(movY) * 8 + 4); //Posiblemente quitar parametros 
-	
+
+	//Para los troncos
+	//int* movX, *movY;
+	//movX = (PINT)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(movX) * 8);
+	//movY = (PINT)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sizeof(movY) * 8 + 4); //Posiblemente quitar parametros
+
+	//wait antes del parto y luego comprobar si hay rana
 	if (funciones.partoRanas(pos)) {
 		nacidas++;
 		posicion = pos;
 		//ABRIR SEMÁFORO CONTROL DE HILOS
-		FERROR(CreateThread(NULL, 0, moverRanas, 0, 0, NULL), NULL, "THREAD_CREATION ERROR");
+		FERROR(CreateThread(NULL, 0, moverRanas, 0, 0, NULL), NULL, "CreateThread() ERROR.\n");
 	} else {
-		Sleep(5000);
-		printf("\nHa ocurrido un error en el parto de las ranas.\n");
+		printf("partoRanas() ERROR.\n");
 		exit(777);
 	}
-	/*
+
+	/* //Esta es la funcion que utilizamos en batracios y la tenemos aqui solo como referencia
 	for (nTroncos = 0; nTroncos < 7; nTroncos++) {
 		//ABRIR MEMORIA COMPARTIDA
 
@@ -181,7 +158,6 @@ void f_criar (int pos) { //Llamará a la función PartoRanas, actualiza las esta
 DWORD WINAPI moverRanas(LPVOID lpParam) {
 	int sentido;
 	int * movX, * movY;
-	int nacimiento = 0;
 	DWORD mem1, mem2;
 
 
@@ -192,7 +168,7 @@ DWORD WINAPI moverRanas(LPVOID lpParam) {
 	*movX = 15 + 16 * posicion;
 	*movY = 0;
 
-	while (noTerminado) //Poner los noterminado=0
+	while (TRUE) //SIN SINCRONIZACIÓN SE MUEVE
 	{
 		mem1 = WaitForSingleObject(ranasMutex, INFINITE); //DEBUG
 		switch (mem1) {
@@ -200,13 +176,12 @@ DWORD WINAPI moverRanas(LPVOID lpParam) {
 					//INICIALIZAR PUNTEROS POSX POSY
 					if ((*movX) < 0 || (*movX) > 79){
 						FERROR(ReleaseMutex(ranasMutex), FALSE, "ReleaseMutex() ERROR.\n");
-
 						(*perdidas)++;
 						(*movY) = -1;
 						(*movX) = -1;
 						break;
 					}
-					
+
 					if (funciones.puedoSaltar(*movX, *movY, ARRIBA)) sentido = ARRIBA;
 					else if (funciones.puedoSaltar(*movX, *movY, DERECHA)) sentido = DERECHA;
 					else if (funciones.puedoSaltar(*movX, *movY, IZQUIERDA)) sentido = IZQUIERDA;
@@ -217,7 +192,6 @@ DWORD WINAPI moverRanas(LPVOID lpParam) {
 						continue;
 					}
 
-					//METER LOS FERROR
 					FERROR(funciones.avanceRanaIni(*movX, *movY),FALSE,"avanceRanaIni() ERROR.\n");
 					FERROR(funciones.avanceRana(movX, movY, sentido), FALSE, "avanceRana() ERROR.\n");
 
@@ -228,13 +202,11 @@ DWORD WINAPI moverRanas(LPVOID lpParam) {
 					mem2 = WaitForSingleObject(ranasMutex, INFINITE);
 					switch (mem2) {
 						case WAIT_OBJECT_0:
-							
+
 							//INICIAR PUNTEROS MOVX MOVY
 
-							if ((*movX) < 0 || (*movX) > 79)
-							{
+							if ((*movX) < 0 || (*movX) > 79){
 								FERROR(ReleaseMutex(ranasMutex), FALSE, "ReleaseMutex() ERROR.\n");
-
 								(*perdidas)++;
 								(*movY) = -1;
 								(*movX) = -1;
@@ -243,7 +215,6 @@ DWORD WINAPI moverRanas(LPVOID lpParam) {
 
 							FERROR(funciones.avanceRanaFin(*movX, *movY), FALSE, "avanceRanaFin() ERROR.\n");
 
-
 							if ((*movY) == 11) {
 								FERROR(ReleaseMutex(ranasMutex), FALSE, "ReleaseMutex() ERROR.\n");
 								(*salvadas)++;
@@ -251,7 +222,8 @@ DWORD WINAPI moverRanas(LPVOID lpParam) {
 								(*movX) = -1;
 								break;
 							}
-							/*
+							/* //Lo teníamos en Batracios, controla el parto de cada madre
+							   //pero como inicioRanas hace eso lo mismo sobra
 							if ((*movY) == 1 && nacimiento == 0)
 							{
 								nacimiento = 1;
@@ -261,7 +233,7 @@ DWORD WINAPI moverRanas(LPVOID lpParam) {
 								if (semop(sem, &sems, 1) == -1) perror("\033[1;31mError semáforo de control de nacimiento de ranaMadre.\033[0m\n");
 							}
 							*/
-							FERROR(ReleaseMutex(ranasMutex), FALSE, "ReleaseMutex() ERROR.\n");
+							FERROR(ReleaseMutex(ranasMutex), FALSE, "ReleaseMutex() ERROR.\n"); //Revisar esto a ver si da problemas
 						case WAIT_ABANDONED:
 							return FALSE;
 					}
@@ -275,32 +247,30 @@ DWORD WINAPI moverRanas(LPVOID lpParam) {
 
 /* ============= Función para tratar argumentos ============= */
 void tratarArg(int argc, char * argv[]) {
+	int param1, param2;
 
-int param1, param2;
+	if(argc != 3) {
+		fprintf(stderr,"%s\n","USO: ./ranas VELOCIDAD VELOCIDAD_PARTO");
+		exit(1);
+	}
 
+	param1 = atoi(argv[1]);
+	param2 = atoi(argv[2]);
 
-if(argc != 3) {
-	fprintf(stderr,"%s\n","USO: ./ranas VELOCIDAD VELOCIDAD_PARTO");
-	exit(1);
-}
+	if (param1 < 0 || param1 > 1000) {
+		fprintf(stderr, "%s\n", "Ha introducido una velocidad incorrecta.\nPor favor, introduzca una velocidad ente 0 y 1000.");
+		exit(1);
+	}
 
-param1 = atoi(argv[1]);
-param2 = atoi(argv[2]);
-
-if (param1 < 0 || param1 > 1000) {
-	fprintf(stderr, "%s\n", "Ha introducido una velocidad incorrecta.\nPor favor, introduzca una velocidad ente 0 y 1000.");
-	exit(1);
-}
-
-if (param2 <= 0) {
-	fprintf(stderr, "%s\n", "Ha introducido un tiempo de partos incorrecto.\nIntroduzca un tiempo mayor de 0.");
-	exit(1);
-}
-
+	if (param2 <= 0) {
+		fprintf(stderr, "%s\n", "Ha introducido un tiempo de partos incorrecto.\nIntroduzca un tiempo mayor de 0.");
+		exit(1);
+	}
 }
 
 /* ============= Función para cargar la biblioteca (DLL) ============= */
 int cargarRanas() {
+	//Funciones BOOL
 	FERROR(funciones.ranasDLL = LoadLibrary(TEXT("ranas.dll")),NULL, "Error: LoadLibrary");
 	FERROR(funciones.avanceRana = (TIPO_AVANCERANA)GetProcAddress(funciones.ranasDLL, "AvanceRana"), NULL, "Error: AvanceRana");
 	FERROR(funciones.avanceRanaFin = (TIPO_AVANCERANAFIN) GetProcAddress(funciones.ranasDLL, "AvanceRanaFin"), NULL, "Error: AvanceRanaFin");
@@ -311,7 +281,7 @@ int cargarRanas() {
 	FERROR(funciones.inicioRanas = (TIPO_INICIORANAS)GetProcAddress(funciones.ranasDLL, "InicioRanas"), NULL, "Error: InicioRanas");
 	FERROR(funciones.partoRanas = (TIPO_PARTORANAS) GetProcAddress(funciones.ranasDLL, "PartoRanas"), NULL, "Error: PartoRanas");
 	FERROR(funciones.puedoSaltar = (TIPO_PUEDOSALTAR) GetProcAddress(funciones.ranasDLL, "PuedoSaltar"), NULL, "Error: PuedoSaltar");
-	
+	//Funciones VOID
 	FERROR(funciones.pausa = (TIPO_PAUSA)GetProcAddress(funciones.ranasDLL, "Pausa"), NULL, "Error: Pausa");
 	FERROR(funciones.printMSG = (TIPO_PRINTMSG)GetProcAddress(funciones.ranasDLL, "PrintMsg"), NULL, "Error: PrintMsg");
 
